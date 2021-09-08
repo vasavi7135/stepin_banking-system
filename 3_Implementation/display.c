dbe623e260082616469ce0532a94750b02ca67c4
vasavi7135 <89580410+vasavi7135@users.noreply.github.com>
2021-09-08 15:45:19 +0530

    Add files via upload

---
+++ 3_Implementation/display.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "bank.h"
+/**
+ * @brief function to display the customers
+ * 
+ * @param list structure of customers
+ * @param s number of customers 
+ */
+void display(struct customer list[120], int s)
+{
+    int i;
+    
+
+    printf("\n\nA/c No\tName\tBalance\n");
+    for (i = 0; i < s; i++)
+    {
+        printf("%d\t%s\t%d\n", list[i].account_no, list[i].name,
+            list[i].balance);
+    } 
+}
