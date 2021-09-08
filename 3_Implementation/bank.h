a29a4e7d6074db1607bfe8a303f7d0a47bd0b18a
vasavi7135 <89580410+vasavi7135@users.noreply.github.com>
2021-09-08 15:41:55 +0530

    Add files via upload

---
+++ 3_Implementation/bank.h
@@ -0,0 +1,35 @@
+/**
+ * @file bank.h
+ * @author pankaj prajapati 
+ * @brief header file
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @brief define bank header file if not defined
+ * 
+ */
+
+#ifndef __BANK_H__
+#define __BANK_H__
+/**
+ * @brief structure containing customer details
+ * 
+ */
+struct customer
+{
+    
+    int account_no;
+    char name[120];
+    int balance;
+};
+void accept(struct customer[], int);
+void display(struct customer[], int);
+int search(struct customer[], int, int);
+void deposit(struct customer[], int, int, int);
+void withdraw(struct customer[], int, int, int);
+#endif
